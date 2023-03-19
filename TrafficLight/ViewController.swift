1d3f5bdd70a8326a3742c3962544e6708b0e9087
Elena <elena.demco@gmail.com>
2023-03-19 17:47:04 +0100

    Code from logging into the app to clicking the button

--- TrafficLight/ViewController.swift
+++ TrafficLight/ViewController.swift
@@ -7,13 +7,58 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+final class ViewController: UIViewController {
+    
+    
+    @IBOutlet var redLight: UIView!
+    @IBOutlet var yellowLight: UIView!
+    @IBOutlet var greenLight: UIView!
+    
+    @IBOutlet var startButton: UIButton!
+    
+    private enum LightTraffic {
+        case red
+        case yellow
+        case green
+    }
+    
+    private var actualLight = LightTraffic.red
+    
+    private let lightTrafficOn = 1.0
+    private let lightTrafficOff = 0.3
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        startButton.layer.cornerRadius = 20
+        
+        redLight.layer.cornerRadius = 65
+        yellowLight.layer.cornerRadius = 65
+        greenLight.layer.cornerRadius = 65
+        
+        redLight.alpha = lightTrafficOff
+        yellowLight.alpha = lightTrafficOff
+        greenLight.alpha = lightTrafficOff
     }
-
-
+    
+    @IBAction func startButtonDidTapped() {
+        
+        startButton.setTitle("NEXT", for: .normal)
+        
+        switch actualLight {
+        case .red:
+            redLight.alpha = lightTrafficOn
+            greenLight.alpha = lightTrafficOff
+            actualLight = .yellow
+        case .yellow:
+            redLight.alpha = lightTrafficOff
+            yellowLight.alpha = lightTrafficOn
+            actualLight = .green
+        case .green:
+            yellowLight.alpha = lightTrafficOff
+            greenLight.alpha = lightTrafficOn
+            actualLight = .red
+        }
+    }
 }
 
